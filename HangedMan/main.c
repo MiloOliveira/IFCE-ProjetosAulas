041fe210205040270170eeed65264d5b3e33754d
Milo Oliveira <miloliveira143@gmail.com>
2025-06-02 15:08:34 -0300

    HangedMan Print

--- HangedMan/main.c
+++ HangedMan/main.c
@@ -1,61 +1,81 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
+#define MaxWords 5
+#define MaxCharacter 12
+
 
 int main() {
-     #define MaxWords 5
-     char WordsBank[3][MaxWords][12] = {
-          {
-           "Mari",
-          "Omori",
-          "Aubrey",
-          "Kel",
-          "Hero"
-          },
-          {
-               "Sweetheart",
-               "Humprey",
-               "Husband",
-               "Unbread",
-               "Chicken",
-          }, 
-          {
-               "Paperclip",
-               "Baseball",
-               "OrangeJoe",
-               "LOLsword",
-               "OragneJack",
-          },
-     };
-
-     /*          
-               "Hero",
-               "Basil",
-               "Aubrey",
-               "Mari",
-               "Sunny"
-
-               "Sweetheart",
-               "Humprey",
-               "Husband",
-               "Unbread",
-               "Chicken",
-          },
-          {
-               "Paperclip",
-               "Baseball",
-               "OrangeJoe",
-               "LOLsword",
-               "OragneJack",
-          },
-     };
-     */
-    for (int ab = 0; ab < 3; ab++) {
-     for (int aa = 0; aa < MaxWords; aa++) {
-          printf("%s \n", WordsBank[ab][aa]);
+     int WordSelected, Win, RoundHitCheck;
+     char Guess;
+     char HangedManWord[MaxCharacter];
+     int HangedManCheck[MaxCharacter];
+     for (int aa = 0; aa < MaxCharacter; aa++) {
+         HangedManCheck[aa] = 0;
      }
-     printf("-------- \n");
-    }
+    int HP;
+    int WordSelectedSize;
+
+     char WordsBank[3][MaxWords][MaxCharacter] = {
+         {
+                "MARI",
+                "OMORI",
+                "AUBREY",
+                "KEL",
+                "HERO"
+            },
+            {
+                "SWEETHEART",
+                "HUMPREY",
+                "HUSBAND",
+                "UNBREAD",
+                "CHICKEN"
+            }, 
+            {
+                "PAPERCLIP",
+                "BASEBALL",
+                "ORANGEJOE",
+                "LOLSWORD",
+                "ORAGNEJACK"
+            },
+     };
      
+    
+    srand(time(0));
+     
+     WordSelected = rand() % MaxWords;
+     strcpy(HangedManWord, WordsBank[2][WordSelected]);
+     WordSelectedSize = strlen(HangedManWord);
+     
+     
+     do {
+        for (int bb = 0; bb < WordSelectedSize; bb++) {
+             if (HangedManCheck[bb] == 0) {
+                 printf ("_ ");
+             } else {
+                 printf("%c ",HangedManWord[bb]);
+             }
+         } 
+         
+        printf("Escolha uma letra \n");
+        scanf(" %c", &Guess);
+        Guess = toupper(Guess);
+        
+        RoundHitCheck = 0;
+        for (int aa = 0; aa < WordSelectedSize; aa++) {
+            if (Guess == HangedManWord[aa]) {
+               HangedManCheck[aa] = 1;
+               RoundHitCheck = 1;
+           }
+        } 
+        
+        if (RoundHitCheck == 0) {
+            //HP--;
+        }
+          
+     } while (HP == 0);
+
      return 0;
 }
