828e5587483d4c600fd49c1ec3d3dfb353f30a30
MiloOliveira <miloliveira143@gmail.com>
2025-07-01 20:42:10 -0300

    Word File

--- HangedMan/main.c
+++ HangedMan/main.c
@@ -1,61 +1,60 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
+#include "Words.txt"
+#define WordSize 12
+
+
+char WordSelected[WordSize];
+int WordLength;
+
+
+
+void WordGet() {
+     int WordsAmount, RandomSelectedWord;
+     
+     FILE* f;
+     f = fopen("Words.txt", "r+");
+     if(f == 0) {
+          printf("Banco de dados de palavras não disponível \n");
+     }
+     fscanf(f,"%d", &WordsAmount);
+     RandomSelectedWord = rand() % WordsAmount;
+     for (int i = 0; i < RandomSelectedWord; i++) {
+          fscanf(f, "%s", &WordSelected);
+     }
+     fclose(f);
+
+     WordLength = strlen(WordSelected);
+}
 
 int main() {
-     #define MaxWords 5
-     char WordsBank[3][MaxWords][12] = {
-          {
-           "Mari",
-          "Omori",
-          "Aubrey",
-          "Kel",
-          "Hero"
-          },
-          {
-               "Sweetheart",
-               "Humprey",
-               "Husband",
-               "Unbread",
-               "Chicken",
-          }, 
-          {
-               "Paperclip",
-               "Baseball",
-               "OrangeJoe",
-               "LOLsword",
-               "OragneJack",
-          },
-     };
+     srand(time(NULL));
+     int LetterDiscovered[WordSize] = {0};
+     char LetterGuess;
+     int Won, Tries;
 
-     /*          
-               "Hero",
-               "Basil",
-               "Aubrey",
-               "Mari",
-               "Sunny"
-
-               "Sweetheart",
-               "Humprey",
-               "Husband",
-               "Unbread",
-               "Chicken",
-          },
-          {
-               "Paperclip",
-               "Baseball",
-               "OrangeJoe",
-               "LOLsword",
-               "OragneJack",
-          },
-     };
-     */
-    for (int ab = 0; ab < 3; ab++) {
-     for (int aa = 0; aa < MaxWords; aa++) {
-          printf("%s \n", WordsBank[ab][aa]);
-     }
-     printf("-------- \n");
-    }
      
+     do {
+          printf("Escreva uma letra para chutar");
+          scanf(" %c", &LetterGuess);
+          for (int i = 0; i < WordLength; i++) {
+               if (WordSelected[i] == LetterGuess) {
+                    LetterDiscovered[i] = 1;
+               }
+          }
+
+          for (int i = 0; i < WordLength; i++){
+               if (LetterDiscovered[i] == 1) {
+                    printf("%c ", WordSelected[i]);
+               } else {
+                    printf("_ ");
+               }
+          }
+
+     } while (Won != 1 || Tries != 0);
+
+
      return 0;
 }
